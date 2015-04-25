70b678e8e50eac3057943dc4c279c66e3cb35184
Martin Bellgardt <bellgardt@i6.informatik.rwth-aachen.de>
2015-04-25 14:11:09 +0200

    Added Scene and LeapMotion hand script

---
+++ Assets/TestHand.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class TestHand : MonoBehaviour {
+
+	protected Controller leap_controller_;
+	public Rigidbody rb;
+	public bool rightHand = false;
+
+	void Awake() {
+		leap_controller_ = new Controller();
+	}
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+		Frame frame = leap_controller_.Frame(); // controller is a Controller object
+		HandList hands = frame.Hands;
+
+		Hand hand;
+		if (rightHand) {
+			hand = hands.Rightmost;
+		} else {
+			hand = hands.Leftmost;
+		}
+
+		if (hand.IsValid) {
+			Vector3 palmPos = hand.PalmPosition.ToUnity () * 0.02f;
+			Quaternion handRotation = hand.Basis.Rotation();
+
+			palmPos.z = 0.0f;
+			rb.MovePosition( palmPos );
+			rb.MoveRotation( handRotation );
+		}
+		//Debug.Log (palmPos.ToString ());
+	}
+}
