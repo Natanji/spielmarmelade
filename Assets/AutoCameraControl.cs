732bd23f7baac46bbff1e708e13694b6b663f057
Martin Bellgardt <bellgardt@i6.informatik.rwth-aachen.de>
2015-04-25 20:12:58 +0200

    bla

--- Assets/AutoCameraControl.cs
+++ Assets/AutoCameraControl.cs
@@ -20,25 +20,26 @@ public class AutoCameraControl : MonoBehaviour {
 		Vector3 thisPos = transform.position;
 		
 		Vector3 direction = ballPos - thisPos;
-		direction.x = 0; // do not look left or right
+		//direction.x = 0; // do not look left or right
 		
-		Vector3 ballPosOnScreen = cameraHdl.WorldToViewportPoint (ballPos);
+		//Vector3 ballPosOnScreen = cameraHdl.WorldToViewportPoint (ballPos);
 		//Debug.Log (ballPosOnScreen);
-		
-		// only if looking up and ball near top screen boundary
-		if (direction.y > 0 ){//&& ballPosOnScreen.y > 0.8) { 
 			
-			float angle = 90f/2f*(9f/16f);
+		float angle = 90f/2f*(9f/16f);
 			
-			//Vector3 threshold = cameraHdl.ViewportToWorldPoint(new Vector3(0.5f, 0.8f, direction.magnitude));
-			Quaternion rotation = new Quaternion();
-			rotation.eulerAngles = new Vector3(angle, 0, 0);
+		//Vector3 threshold = cameraHdl.ViewportToWorldPoint(new Vector3(0.5f, 0.8f, direction.magnitude));
+		Quaternion rotation = new Quaternion();
+
+		Quaternion parentRotation = transform.parent.rotation;
+
+		rotation.eulerAngles = new Vector3(angle, 0, 0);
 			
-			Vector3 adjustedDirection = rotation*direction;
+		Vector3 adjustedDirection = rotation*direction;
 			
-			Quaternion lookAtRotation = Quaternion.LookRotation (adjustedDirection, Vector3.up);
-			
-			transform.rotation = lookAtRotation;
-		} 
+		Quaternion lookAtRotation = Quaternion.LookRotation (adjustedDirection, Vector3.up);
+
+		if (direction.y > 0 ){
+			//transform.rotation = lookAtRotation;
+		}
 	}
 }
