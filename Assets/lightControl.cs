fbedb09256bed45a0885f6f9815b2dd4043e915a
Martin Bellgardt <bellgardt@i6.informatik.rwth-aachen.de>
2015-04-26 19:10:45 +0200

    made light control work

--- Assets/lightControl.cs
+++ Assets/lightControl.cs
@@ -4,19 +4,24 @@ using System.Collections;
 public class lightControl : MonoBehaviour {
 
 	protected Renderer r;
+	public GameObject world;
+	public Light light;
 
 	// Use this for initialization
 	void Start () {
 
 		r = gameObject.GetComponentInChildren<Renderer>();
 
-		r.material.SetColor ("_EmissionColor", Color.red);
+		r.material.SetColor ("_EmissionColor", Color.black);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		float progress = world.GetComponent<SplineWalker> ().getProgress();
 
+		Color eColor = new Color (progress / 120.0f, 0.0f, 0.0f);
 
-		r.material.SetColor ("_EmissionColor", Color.red);
+		light.color = eColor;
+		r.material.SetColor ("_EmissionColor", eColor);
 	}
 }
