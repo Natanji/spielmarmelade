28d3174609cf19657436876e57d24bd2f428cdd2
Martin Bellgardt <bellgardt@i6.informatik.rwth-aachen.de>
2015-04-26 19:54:59 +0200

    Made light pulsate.

--- Assets/lightControl.cs
+++ Assets/lightControl.cs
@@ -18,8 +18,11 @@ public class lightControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float progress = world.GetComponent<SplineWalker> ().getProgress();
+		float maxProgress = world.GetComponent<SplineWalker> ().duration;
 
-		Color eColor = new Color (progress / 120.0f, 0.0f, 0.0f);
+		float factor = (progress / maxProgress * (Mathf.Sin(progress*2.0f) + 1.0f) * 0.25f) + (progress / maxProgress * 0.5f);
+
+		Color eColor = new Color (factor, 0.0f, 0.0f);
 
 		light.color = eColor;
 		r.material.SetColor ("_EmissionColor", eColor);
